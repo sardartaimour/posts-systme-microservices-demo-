3f387f14cb312bc059becbf5f91b65e93f461206
taimour-niche <taimour.nicheacademy@nxvt.com>
2023-01-11 12:01:00 +0500

    ...

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,10 +6,12 @@ const PORT = 3300;
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
 app.post('/events', async (req, res, next) => {
 
     try {
         const event = req.body;
+        events.push(event);
 
         await axios.post('http://localhost:3100/events', event); // comment service
         await axios.post('http://localhost:3200/events', event); // post service
@@ -22,6 +24,10 @@ app.post('/events', async (req, res, next) => {
     }
 
 });
+
+app.get('/events', (req, res) => {
+    res.status(200).send(events);
+})
 
 app.listen(PORT, () => {
     console.log('Event Bus is listening on port: ', PORT)
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const axios = require('axios');
 
 const PORT = 3400;
 const app = express();
@@ -9,52 +10,61 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', async (req, res, next) => {
-    try {
-        res.send(posts);
-    } catch(e) {
-        next(e)
-    }
+app.get("/posts", async (req, res, next) => {
+  try {
+    res.send(posts);
+  } catch (e) {
+    next(e);
+  }
 });
 
-app.post('/events', async (req, res, next) => {
+app.post("/events", async (req, res, next) => {
+  try {
+    const { type, data } = req.body;
+    handleEvent(type, data);
 
-    try {
-
-        const { type, data } = req.body;
-        const id = data && data.id ? data.id : null;
-        const comment = data && data.comment ?  data.comment : '';
-        const postId = data && data.postId ? data.postId : null;
-        const status = data && data.status ? data.status : 'Pending';
-        const title = data && data.title ? data.title : '';
-
-        switch(type) {
-            case 'PostCreated':
-                posts[id] = {
-                    id, title, comments: []
-                }
-                break;
-            case 'CommentCreated':
-                const post  = posts[postId];
-                post.comments.push({ id, comment, postId, status });
-                break;
-            case 'CommentUpdated':
-                const _post  = posts[postId];
-                const _comment = _post.comments.find(com => com.id === id);
-                _comment.comment = comment;
-                _comment.status = status;
-                break;
-            default:
-                break;
-        }
-        
-        res.send({ status: 'Ok' });
-    } catch(e) {
-        next(e)
-    }
-
+    res.send({ status: "Ok" });
+  } catch (e) {
+    next(e);
+  }
 });
 
-app.listen(PORT, () => {
-    console.log('Query service is listening on port: ', PORT)
+const handleEvent = (type, data) => {
+  const id = data && data.id ? data.id : null;
+  const comment = data && data.comment ? data.comment : "";
+  const postId = data && data.postId ? data.postId : null;
+  const status = data && data.status ? data.status : "Pending";
+  const title = data && data.title ? data.title : "";
+
+  switch (type) {
+    case "PostCreated":
+      posts[id] = {
+        id,
+        title,
+        comments: [],
+      };
+      break;
+    case "CommentCreated":
+      const post = posts[postId];
+      post.comments.push({ id, comment, postId, status });
+      break;
+    case "CommentUpdated":
+      const _post = posts[postId];
+      const _comment = _post.comments.find((com) => com.id === id);
+      _comment.comment = comment;
+      _comment.status = status;
+      break;
+    default:
+      break;
+  }
+};
+
+app.listen(PORT, async () => {
+  console.log("Query service is listening on port: ", PORT);
+
+  const res = await axios.get('http://localhost:3300/events');
+
+  for (let event of res.data) {
+    handleEvent(event.type, event.data);
+  }
 });
